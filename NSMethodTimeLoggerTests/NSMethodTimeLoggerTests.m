014392502a324e85e7ac10967df5923b07a49e34
Rahul Nadella <rahulnadella@yahoo.com>
2013-06-05 16:03:15 -0400

    Initial Commit

--- NSMethodTimeLoggerTests/NSMethodTimeLoggerTests.m
+++ NSMethodTimeLoggerTests/NSMethodTimeLoggerTests.m
@@ -47,10 +47,35 @@
     [super tearDown];
 }
 
+- (int) challenge
+{
+    int h = 0;
+    int i = 20;
+    int j = 25;
+    
+    int k = (i > j) ? 10: 5;
+    
+    if ( 5 < j - k) {
+        printf("The first expression is true.");
+    } else if ( j > i) {
+        printf("The second expression is true.");
+    } else {
+        printf("Neither expression is true.");
+    }
+    
+    while (h < 10000)
+    {
+        printf("The while loop is still running.");
+        ++h;
+    }
+    
+    return 0;
+}
+
 - (void)testNSMethodTimeLogger
 {
     [self logTimeElapsedPerMethod:^{
-        for(int i = 0 ; i < 1000000000; i ++);
+        [self challenge];
     }
     withMethodDescription:@"For Loop"];
 }
